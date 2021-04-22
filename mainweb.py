dcb28d9412d9e2653872b71d83f8f16b1ce3b6eb
yhuen24 <56414768+yhuen24@users.noreply.github.com>
2021-04-22 20:41:17 +0100

    Initial commit

---
+++ mainweb.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+import json
+
+data = json.load(open("data.json"))
+app = Flask(__name__)
+result = " "
+holder = "Enter word"
+
+
+@app.route('/', methods=["GET", "POST"])
+def main():
+    if request.method == "POST":
+        text = str(request.form.get('word')).lower()
+        if text is None:
+            prompt = "Enter a word"
+            return render_template("home.html", result=prompt, holder=holder)
+        if text in data:
+            string = str(data[text])[1:-1]
+            return render_template("home.html", result=string, holder=text)
+        else:
+            prompt = '"' + text + '" does not exist in the Dictionary'
+            return render_template("home.html", result=prompt, holder=text)
+
+    return render_template("home.html", result=result, holder=holder)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
